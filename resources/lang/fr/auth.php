b1c7ab73e8d3363821609a5e9982a2f7a4afb63c
Grégory Van Ossel <vanosselgregory@gmail.com>
2021-09-21 21:34:39 +0200

    Ajout traductions FR

--- resources/lang/fr/auth.php
+++ resources/lang/fr/auth.php
@@ -1,20 +1,18 @@
 <?php
 
+/*
+|--------------------------------------------------------------------------
+| Authentication Language Lines
+|--------------------------------------------------------------------------
+|
+| The following language lines are used during authentication for various
+| messages that we need to display to the user. You are free to modify
+| these language lines according to your application's requirements.
+|
+*/
+
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Authentication Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used during authentication for various
-    | messages that we need to display to the user. You are free to modify
-    | these language lines according to your application's requirements.
-    |
-    */
-
-    'failed' => 'Ces données ne correspondent pas.',
-    'password' => 'Le mot de passe est incorrect.',
-    'throttle' => 'Trop d\'essais. Réessayez dans :seconds secondes.',
-
+    'failed'   => 'Ces identifiants ne correspondent pas à nos enregistrements.',
+    'password' => 'Le mot de passe fourni est incorrect.',
+    'throttle' => 'Tentatives de connexion trop nombreuses. Veuillez essayer de nouveau dans :seconds secondes.',
 ];
--- resources/views/components/auth-validation-errors.blade.php
+++ resources/views/components/auth-validation-errors.blade.php
@@ -2,8 +2,8 @@
 
 @if ($errors->any())
     <div {{ $attributes }}>
-        <div class="font-medium text-red-600">
-            {{ __('Whoops! Something went wrong.') }}
+        <div class="font-medium text-red-600 mt-2">
+            {{ __('Oups! Nous avons un problème...') }}
         </div>
 
         <ul class="mt-3 list-disc list-inside text-sm text-red-600">
