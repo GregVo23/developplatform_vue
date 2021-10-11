54d5ac9a7d4e6aa78a9ef74280e90eeee2d8974c
Grégory Van Ossel <vanosselgregory@gmail.com>
2021-10-11 16:41:42 +0200

    Ajout liens footer

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -1,21 +1,21 @@
     <!-- Navigation Links -->
     <ul class="mt-4 space-y-4">
-        <li><a class="text-gray-500 hover:text-indigo-500" href="#">
+        <li><a class="text-gray-500 hover:text-indigo-500" href="/accueil">
             {{ __('Accueil') }}</a>
         </li>
-        <li><a class="text-gray-500 hover:text-indigo-500" href="#">
+        <li><a class="text-gray-500 hover:text-indigo-500" href="/projets">
             {{ __('Rechercher') }}</a>
         </li>
-        <li><a class="text-gray-500 hover:text-indigo-500" href="#">
+        <li><a class="text-gray-500 hover:text-indigo-500" href="/nouveau">
             {{ __('Demander') }}</a>
         </li>
-        <li><a class="text-gray-500 hover:text-indigo-500" href="#">
+        <li><a class="text-gray-500 hover:text-indigo-500" href="/favoris">
             {{ __('Favoris') }}</a>
         </li>
-        <li><a class="text-gray-500 hover:text-indigo-500" href="#">
+        <li><a class="text-gray-500 hover:text-indigo-500" href="/demandes">
             {{ __('Mes demandes') }}</a>
         </li>
-        <li><a class="text-gray-500 hover:text-indigo-500" href="#">
+        <li><a class="text-gray-500 hover:text-indigo-500" href="/offres">
             {{ __('Mes offres') }}</a>
         </li>
     </ul>
